c430191dad41e5f3d9171a039bf28c48589940a1
darkHarry <harry.mecwan91@gmail.com>
2020-12-01 04:46:21 +0530

    Add Edit Card functionality with overlay

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 class Note {
     constructor(title, text) {
-        const titleEle = document.createElement("div"); 
-        const textEle = document.createElement("div");
+        const titleEle = document.createElement("input");
+        const textEle = document.createElement("textarea");
 
-        titleEle.innerHTML = title;
-        textEle.innerHTML = text;
+        titleEle.setAttribute("type", "text");
+        titleEle.setAttribute("maxlength", "25");
+        titleEle.setAttribute("disabled", "");
+        textEle.setAttribute("disabled", "");
+
+        titleEle.value = title;
+        textEle.value = text;
         titleEle.classList.add("note-title");
         textEle.classList.add("note-text");
 
@@ -13,15 +18,19 @@ class Note {
         timeEle.setAttribute("datetime", time.toUTCString());
         timeEle.innerHTML = time.toUTCString().split(" ").slice(0, 4).join(" ");
 
+        const btns = document.createElement("div");
         const editBtn = document.createElement("button");
         const deleteBtn = document.createElement("button");
+        btns.appendChild(editBtn);
+        btns.appendChild(deleteBtn);
+        btns.classList.add("note-btns");
 
         editBtn.innerHTML = "Edit";
         deleteBtn.innerHTML = "Delete";
 
         this.note = document.createElement("div");
         this.note.classList.add("note");
-        for (let child of [timeEle, titleEle, textEle,editBtn, deleteBtn]) {
+        for (let child of [timeEle, titleEle, textEle, btns]) {
             this.note.appendChild(child);
         }
 
@@ -31,6 +40,31 @@ class Note {
 
         deleteBtn.addEventListener("click", () => {
             this.note.parentElement.removeChild(this.note);
+            document.querySelector(".overlay").style.display = "none";
+        });
+
+        editBtn.addEventListener("click", () => {
+            const title = this.note.querySelector(".note-title");
+            const text = this.note.querySelector(".note-text");
+            const overlay = document.querySelector(".overlay");
+            if (editBtn.innerHTML === "Edit") {
+                this.note.classList.add("note-overlay");
+                overlay.style.display = "block";
+                title.setAttribute("placeholder", "Title");
+                text.setAttribute("placeholder", "Take a note...");
+                title.removeAttribute("disabled");
+                text.removeAttribute("disabled");
+                editBtn.innerHTML = "Done";
+                title.focus();
+            } else if (editBtn.innerHTML === "Done") {
+                this.note.classList.remove("note-overlay");
+                title.setAttribute("disabled", "");
+                text.setAttribute("disabled", "");
+                editBtn.innerHTML = "Edit";
+                title.removeAttribute("placeholder");
+                text.removeAttribute("placeholder");
+                overlay.style.display = "none";
+            }
         });
     }
 }
@@ -47,12 +81,12 @@ class Notes {
     }
 }
 
-const removeDivs = function(text) {
-    return text.split("<div>").join("");
+const replaceNewLines = function(text) {
+    return text.replace(/\\n/g, String.fromCharCode(13, 10));
 }
 
 const processText = function(text) {
-    const processedText = removeDivs(text);
+    const processedText = replaceNewLines(text);
     return processedText;
 }
 
@@ -62,10 +96,10 @@ document.addEventListener(
         let notes = new Notes();
         const titleInput = document.querySelector(".input-title");
         const textInput = document.querySelector(".input-text");
-        const submitBtn = document.querySelector("#submit-btn");
+        const submitBtn = document.querySelector(".submit-btn");
 
         submitBtn.addEventListener("click", function() {
-            notes.add(titleInput.value, processText(textInput.innerHTML));
+            notes.add(titleInput.value, processText(textInput.innerText));
         }); 
 
         textInput.addEventListener("focus", function(event) {
