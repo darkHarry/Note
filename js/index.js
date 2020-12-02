c548e8d553871840fdd7e16cd8cb12275b415697
darkHarry <harry.mecwan91@gmail.com>
2020-12-03 01:28:42 +0530

    Add Button Icons

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 class Note {
     constructor(title, text) {
+        // Create Card Title and Text Elements
         const titleEle = document.createElement("input");
         const textEle = document.createElement("textarea");
 
+        // Set attributes to Title and Text
         titleEle.setAttribute("type", "text");
         titleEle.setAttribute("maxlength", "25");
         titleEle.setAttribute("disabled", "");
@@ -13,11 +15,13 @@ class Note {
         titleEle.classList.add("note-title");
         textEle.classList.add("note-text");
 
+        // Create card addition timestamp
         const timeEle = document.createElement("time");
         const time = new Date();
         timeEle.setAttribute("datetime", time.toUTCString());
         timeEle.innerHTML = time.toUTCString().split(" ").slice(0, 4).join(" ");
 
+        // create card button elements
         const btns = document.createElement("div");
         const editBtn = document.createElement("button");
         const deleteBtn = document.createElement("button");
@@ -25,42 +29,52 @@ class Note {
         btns.appendChild(deleteBtn);
         btns.classList.add("note-btns");
 
-        editBtn.innerHTML = "Edit";
-        deleteBtn.innerHTML = "Delete";
+        // add material icons to card buttons
+        const editIcon = document.createElement("span");
+        editIcon.classList.add("material-icons");
+        const deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("material-icons");
+        editIcon.innerHTML = "edit";
+        deleteIcon.innerHTML = "delete";
+        editBtn.appendChild(editIcon);
+        deleteBtn.appendChild(deleteIcon);
 
+        // add all card elements to card
         this.note = document.createElement("div");
         this.note.classList.add("note");
         for (let child of [timeEle, titleEle, textEle, btns]) {
             this.note.appendChild(child);
         }
 
-        // create a background color for note
+        // create a background color for card
         const color = `hsl(${Math.ceil(360*Math.random())},${Math.ceil(71*Math.random())+24}%,${Math.ceil(11*Math.random())+80}%)`;
         this.note.style.backgroundColor = color;
 
+        // attach delete event for card
         deleteBtn.addEventListener("click", () => {
             this.note.parentElement.removeChild(this.note);
             document.querySelector(".overlay").style.display = "none";
         });
 
+        // attach overlay editor for card
         editBtn.addEventListener("click", () => {
             const title = this.note.querySelector(".note-title");
             const text = this.note.querySelector(".note-text");
             const overlay = document.querySelector(".overlay");
-            if (editBtn.innerHTML === "Edit") {
+            if (editIcon.innerHTML === "edit") {
                 this.note.classList.add("note-overlay");
                 overlay.style.display = "block";
                 title.setAttribute("placeholder", "Title");
                 text.setAttribute("placeholder", "Take a note...");
                 title.removeAttribute("disabled");
                 text.removeAttribute("disabled");
-                editBtn.innerHTML = "Done";
+                editIcon.innerHTML = "check_circle";
                 title.focus();
-            } else if (editBtn.innerHTML === "Done") {
+            } else if (editIcon.innerHTML === "check_circle") {
                 this.note.classList.remove("note-overlay");
                 title.setAttribute("disabled", "");
                 text.setAttribute("disabled", "");
-                editBtn.innerHTML = "Edit";
+                editIcon.innerHTML = "edit";
                 title.removeAttribute("placeholder");
                 text.removeAttribute("placeholder");
                 overlay.style.display = "none";
